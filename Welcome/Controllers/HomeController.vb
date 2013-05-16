3f06de621bd9c836bfa7cf5012fad75546c835b8
sanababeyeh <eng_babeyeh@windowslive.com>
2013-05-16 13:24:41 +0300

    sssdsds

---
+++ Welcome/Controllers/HomeController.vb
@@ -0,0 +1,13 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "welcome to my cloud "
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        Return View()
+    End Function
+End Class
